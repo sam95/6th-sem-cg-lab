dcf5ee8a783bcf92bd6a83b0bd238ead7d7e328c
Sameera S <sameera1595@gmail.com>
2015-05-27 11:42:03 +0530

    cg lab

--- 5.c
+++ 5.c
@@ -129,10 +129,9 @@ void display()
 	glFlush();
 }
 
-
-
-void main()
+void main(int argc,char **argv)
 {
+	glutInit(&argc,argv);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(500,500);
 	glutCreateWindow("CLIPPING ALGORITHM");
